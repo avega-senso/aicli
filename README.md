310dcbd60614b8cf1e3a84144b1129a121502bac
Robert Dahlborg <robert@dahlborg.com>
2023-09-01 14:25:39 +0200

    chore(README.md): update project name and add overview and features section
    fix(aicli.py): change get_response_from_gpt3 function to accept model and temperature parameters for flexibility
    
    feat(aicli.py): add sanitize_message function to redact sensitive information from error messages
    
    feat(aicli.py): add model, temperature, and sanitize options to main function for customization
    
    feat(aicli.py): add argparse options for model, temperature, and sanitize

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# aicli - Interact with OpenAI from the cli
+# AI CLI tool - Interact with OpenAI from the cli
 
-`aicli` is a simple command-line script to interact with OpenAI's GPT-3.5 model. You can provide prompts either directly as command-line arguments, or pipe them into the script from other sources.
+Overview
+The AI CLI tool provides a simple and direct interface to interact with OpenAI's models from the command line. By leveraging the powerful capabilities of OpenAI, you can quickly obtain responses for any given prompt.
+
+Whether you're integrating with your workflow or just wanting to have a conversation, this CLI tool simplifies the process. It's designed with flexibility in mind, allowing both interactive sessions and the ability to pipe in data for automated tasks.
+
+## Features
+### Direct Interactivity
+Launch the tool and immediately start a conversation with OpenAI.
+### Support for Piped Data
+Use other tools in conjunction with this CLI to send data directly into the script.
+### Sanitization Feature
+The sanitization feature in this script allows users to redact potentially sensitive information from the provided input. It uses a set of predefined patterns (like IP addresses, email addresses, URLs, and Unix file paths) to identify and replace such information with the placeholder `[REDACTED]`. This ensures that confidential or personal data isn't inadvertently exposed or sent in its raw form.
+
+`AI CLI Tool` is a simple command-line script to interact with OpenAI's GPT-3.5 model. You can provide prompts either directly as command-line arguments, or pipe them into the script from other sources.
 
 ## Prerequisites
 
--- aicli.py
+++ aicli.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import openai
 import sys
 import argparse
+import re
 
 # Load .env file if it exists
 load_dotenv()
@@ -11,46 +12,64 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_base = os.getenv("OPENAI_API_BASE")
 
-def get_response_from_gpt3(prompt):
+# Dynamically load patterns from the .env file
+patterns = [os.getenv(f'PATTERN_{i}') for i in range(1, 100) if os.getenv(f'PATTERN_{i}')]
+
+def sanitize_message(error_msg):
+    sanitized_msg = error_msg
+    for pattern in patterns:
+        sanitized_msg = re.sub(pattern, '[REDACTED]', sanitized_msg)
+    return sanitized_msg
+
+
+def get_response_from_gpt3(prompt, model='gpt-3.5-turbo', temperature=0):
     messages = [{'role': 'user', 'content': prompt}]
     response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
+        model=model,
         messages=messages,
-        temperature=0,
-        stream=True  # Assuming the API supports streaming
+        temperature=temperature,
+        stream=True
     )
     return response
 
-def main(prompt=None, is_interactive=True):
-    if prompt:
-        response = get_response_from_gpt3(prompt)
+def main(prompt=None, model='gpt-3.5-turbo', temperature=0, is_interactive=True, sanitize=False):
+    if sanitize:
+        prompt = sanitize_message(prompt)
+    try:
+        response = get_response_from_gpt3(prompt, model, temperature)
         for chunk in response:
             if 'choices' in chunk and len(chunk['choices']) > 0 and 'content' in chunk['choices'][0]['delta']:
-                print(chunk['choices'][0]['delta']['content'], end='', flush=True)  # Print chunk content on the same line immediately
+                content = chunk['choices'][0]['delta']['content']
+                print(content, end='', flush=True)
         print()
-        sys.exit(0)  # Terminate the script after printing the response
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit(0)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
 
 def get_input_from_std_streams():
-    # Check for input from stdin
     input_data = sys.stdin.read().strip()
     if not input_data:
-        # If nothing in stdin, try reading from stderr
         input_data = sys.stderr.read().strip()
     return input_data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="ai", description="Interact with OpenAI.")
     parser.add_argument('prompt', type=str, nargs='?', default=None, help="Initial prompt to provide to OpenAI.")
+    parser.add_argument('--model', type=str, default='gpt-3.5-turbo', help="Model to use for the interaction.")
+    parser.add_argument('--temperature', type=float, default=0, help="Temperature setting for the model's response.")
+    parser.add_argument('--sanitize', action="store_true", help="Sanitize the message to redact sensitive information.")
+
     args = parser.parse_args()
     
-    # If a prompt argument was provided
     if args.prompt:
-        main(prompt=args.prompt)
-    # If data is piped into the script
+        main(prompt=args.prompt, model=args.model, temperature=args.temperature, sanitize=args.sanitize)
     elif not sys.stdin.isatty() or not sys.stderr.isatty():
         initial_prompt = get_input_from_std_streams()
         if initial_prompt:
-            main(prompt=initial_prompt, is_interactive=False)
-    else:  # If the script is run interactively
+            main(prompt=initial_prompt, model=args.model, temperature=args.temperature, sanitize=args.sanitize, is_interactive=False)
+    else:
         initial_prompt = input("AI% ")
-        main(prompt=initial_prompt)
+        main(prompt=initial_prompt, model=args.model, temperature=args.temperature, sanitize=args.sanitize)
