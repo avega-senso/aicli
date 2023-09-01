3550f06de8c8ec3798fda8d0569eeb9e350b2a3a
Robert Dahlborg <robert@dahlborg.com>
2023-09-01 12:24:54 +0200

    chore(.gitignore): add .env to the gitignore file refactor(aicli.py): simplify code by removing unnecessary content variable and loop duplication feat(aicli.py): add support for providing initial prompt as a command line argument feat(aicli.py): change prompt input message to "AI% " for better user experience

--- aicli.py
+++ aicli.py
@@ -20,32 +20,11 @@ def get_response_from_gpt3(prompt):
 def main(prompt=None, is_interactive=True):
     if prompt:
         response = get_response_from_gpt3(prompt)
-        content = ""
         for chunk in response:
             if 'choices' in chunk and len(chunk['choices']) > 0 and 'content' in chunk['choices'][0]['delta']:
-                content += chunk['choices'][0]['delta']['content']
-        print(content)
-    
-    if not is_interactive:
-        return
-
-    try:
-        while True:
-            user_input = input("Enter your question or prompt: ")
-            
-            # Check for exit command
-            if user_input.lower() in ['exit', 'quit']:
-                print("Exiting...")
-                break
-
-            response = get_response_from_gpt3(user_input)
-            content = ""
-            for chunk in response:
-                if 'choices' in chunk and len(chunk['choices']) > 0 and 'content' in chunk['choices'][0]['delta']:
-                    content += chunk['choices'][0]['delta']['content']
-            print(content)
-    except KeyboardInterrupt:
-        print("\nExiting...")  # Print an exit message when Ctrl+C is pressed
+                print(chunk['choices'][0]['delta']['content'], end='', flush=True)  # Print chunk content on the same line immediately
+        print()
+        sys.exit(0)  # Terminate the script after printing the response
 
 def get_input_from_std_streams():
     # Check for input from stdin
@@ -57,7 +36,7 @@ def get_input_from_std_streams():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Interact with GPT-3.")
-    parser.add_argument('--prompt', type=str, default=None, help="Initial prompt to provide to GPT-3.")
+    parser.add_argument('prompt', type=str, nargs='?', default=None, help="Initial prompt to provide to GPT-3.")
     args = parser.parse_args()
     
     # If a prompt argument was provided
@@ -69,6 +48,5 @@ if __name__ == "__main__":
         if initial_prompt:
             main(prompt=initial_prompt, is_interactive=False)
     else:  # If the script is run interactively
-        initial_prompt = input("Enter initial prompt: ")
+        initial_prompt = input("AI% ")
         main(prompt=initial_prompt)
-
